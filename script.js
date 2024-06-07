900fe3eaff4e8f83aa9af5f6c03cfd494a98718b
Nasirisl <islamnasir40@gmail.com>
2024-06-07 23:39:19 +0500

    first upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// BARISH
+
+var badal = true;
+if(badal){
+    console.log("Barish coming soon in peshawar")
+
+}
+if(!badal){
+    console.log("Barish nahi hoge ")
+}
+
+// BRT CARD
+
+// var brtcard = +prompt("Check  your card Balance")
+// if(brtcard>=50){
+//     console.log("You can travel")
+// }
+// if(brtcard<50){
+//     console.log("Plz recharge your card")
+
+// }
+
+// Burger lena he.
+
+// const burger = 100 ;
+// const paisa = +prompt("Jaib me pese")
+// if(paisa >= burger){
+//     console.log(" LE Leeeeee !")
+// }
+// else{
+//     console.log("Chill kar");
+// }
+
+
+// Even odd  using if 
+
+// var number = +prompt("Enter your number ")
+// remainder= number%2;
+// if(remainder==0){
+//     console.log("your number is even");
+// }
+// else{
+//     console.log("Your number is ODD")
+// }
+
+// total bill
+ 
